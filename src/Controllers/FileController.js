527841dee73566a7aac09f96a76e579389703b23
Ayomide-Daniel <ayomidedaniel00@gmail.com>
2022-02-26 22:57:25 +0100

    feat: update filecontroller

--- src/Controllers/FileController.js
+++ src/Controllers/FileController.js
@@ -4,5 +4,6 @@ exports.getSomeFile = (req, res) => {
   const { key, field } = req.params;
   const readStream = getFile(key, field);
   res.setHeader("Cross-Origin-Resource-Policy", "same-site");
+  res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
   readStream.pipe(res);
 };
