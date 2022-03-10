6be334515eadcec4c08827ae8c6e5da0784a869f
Ayomide-Daniel <ayomidedaniel00@gmail.com>
2022-03-10 23:29:30 +0100

    feat: easy crash patch

--- src/Middlewares/verifyUser.js
+++ src/Middlewares/verifyUser.js
@@ -4,24 +4,29 @@ const { jwtSecret } = require("../../config/jwt.config");
 const User = require("../../models/User");
 
 exports.verifyUser = async (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
+  try {
+    const token = req.headers.authorization.split(" ")[1];
 
-  if (typeof token != undefined) {
-    jwt.verify(token, jwtSecret, async (err, decoded) => {
-      if (err) {
-        errorResponse(res, 401, "User unauthenticated", null);
-      } else {
-        const email = decoded.user.email;
-        const user = await User.findOne({ email });
-        if (!user) {
-          errorResponse(res, 401, "TF???", null);
+    if (typeof token != undefined) {
+      jwt.verify(token, jwtSecret, async (err, decoded) => {
+        if (err) {
+          errorResponse(res, 401, "User unauthenticated", null);
         } else {
-          req.user = user;
-          next();
+          const email = decoded.user.email;
+          const user = await User.findOne({ email });
+          if (!user) {
+            errorResponse(res, 401, "TF???", null);
+          } else {
+            req.user = user;
+            next();
+          }
         }
-      }
-    });
-  } else {
+      });
+    } else {
+      errorResponse(res, 401, "User is unauthenticated", null);
+    }
+  } catch (error) {
+    console.error(error);
     errorResponse(res, 401, "User is unauthenticated", null);
   }
 };
